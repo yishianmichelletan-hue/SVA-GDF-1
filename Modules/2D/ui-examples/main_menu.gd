a3b5cf72b3a2dfc6c262a9ef8dc0b597035635fe
yishianmichelletan-hue <yishianmichelletan@gmail.com>
2026-03-26 22:58:04 -0400

    buttons and ui

--- Modules/2D/ui-examples/main_menu.gd
+++ Modules/2D/ui-examples/main_menu.gd
@@ -2,3 +2,7 @@ extends Control
 #const LEVEL_1 = preload("res://level1.tscn")
 func _on_button_button_down():
 	get_tree().change_scene_to_file("res://level1.tscn")
+
+
+func _on_how_to_play__rules_button_down() -> void:
+	get_tree().change_scene_to_file("res://r_ules.tscn")
